0202d8fe052a9aecbe8cfe3ae2c88ca0209bff34
tx991020 <1059622332@qq.com>
2020-09-11 08:14:11 +0800

    update clap

--- a34/src/main.rs
+++ a34/src/main.rs
@@ -1,3 +1,23 @@
+use clap::{load_yaml, App};
+
 fn main() {
-    println!("Hello, world!");
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml).get_matches();
+
+    // Same as previous examples...
+    match matches.subcommand_name() {
+        Some("test") => println!("'myapp add' was used"),
+        Some("clone") => {
+            println!("{}", "clone");
+        } // clone was used
+        Some("push") => {
+            println!("{}", "push");
+        } // push was used
+        Some("commit") => {
+            println!("{}", "commit");
+        } // commit was used
+
+        _ => {}
+    }
 }
--- sh/src/main.rs
+++ sh/src/main.rs
@@ -1,10 +1,18 @@
+use anyhow::{Error, Result};
 use shellfn::shell;
-use std::error::Error;
 
 #[shell]
-fn list_modified(dir: &str) -> Result<impl Iterator<Item = String>, Box<Error>> {
+fn list_modified(dir: &str) -> Result<impl Iterator<Item = String>, Error> {
     r#"
     cd $DIR
     git status | grep '^\s*modified:' | awk '{print $2}'
 "#
+}
+
+fn main() -> Result<()> {
+    let result = list_modified("/Users/andy/CLionProjects/todo/sh")?;
+    for i in result {
+        println!("{}", i);
+    }
+    Ok(())
 }
